df999957e8bab9ac9b1506e44202fc9d7bfb4d77
sdeleon <steven.deleonfeliz@gmail.com>
2023-08-14 16:55:40 -0400

    Responsive app

--- src/app/modules/shared/sidebar/sidebar.component.ts
+++ src/app/modules/shared/sidebar/sidebar.component.ts
@@ -4,7 +4,9 @@ import { GifsService } from '../../gifs/services/gifs.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  
+
 })
 export class SidebarComponent {
 
