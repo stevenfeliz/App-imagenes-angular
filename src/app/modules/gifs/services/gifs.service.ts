3b88deb5ce8eaec41c57e3d3934e794f65acb259
sdeleon <steven.deleonfeliz@gmail.com>
2023-08-15 00:21:53 -0400

    localstorage, btndelete and more

--- src/app/modules/gifs/services/gifs.service.ts
+++ src/app/modules/gifs/services/gifs.service.ts
@@ -14,7 +14,10 @@ export class GifsService {
     return [...this._historial];
   }
 
+
   constructor(private http:HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
 
   }
 
@@ -26,14 +29,19 @@ export class GifsService {
     if(!this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+      
+      
 
     }
     this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=UC1rk0FpF6stB3wN2CDpKjuMzM3VcAtQ&q=${query}&limit=10`)
     .subscribe((resp:any) =>{
       this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      
       
     })
-    console.log(this.resultados)
+    
   }
 
   
--- src/app/modules/shared/sidebar/sidebar.component.ts
+++ src/app/modules/shared/sidebar/sidebar.component.ts
@@ -17,5 +17,28 @@ export class SidebarComponent {
   get historial(){
     return this.gifsService.historial
   }
+  
+  selecion(item:any){
+    console.log(item)
+
+    this.gifsService.buscarGifs(item);
+  }
  
+  delete(event:any){
+   const data = event.target.id
+   let localStorageData = localStorage.getItem('historial')
+
+   if (localStorageData !== null) {
+    // Paso 1: Convierte los datos en un array
+    let dataArray: string[] = JSON.parse(localStorageData);
+
+    let nuevoArray = dataArray.filter(valor => valor !== data);
+
+   
+    localStorage.setItem('historial', JSON.stringify(nuevoArray));
+
+    window.location.reload();
+   
+   }
+  }
 }
